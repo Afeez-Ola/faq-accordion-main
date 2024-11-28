d8c9d159cce9922a0f8790420d665acb6e71c57b
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-28 03:24:45 +0100

    fighting for my life!

--- script/main.js
+++ script/main.js
@@ -1,40 +1,41 @@
 const expandButtons = document.querySelectorAll(".expand_btn");
 const closeButtons = document.querySelectorAll(".close_btn");
 const answers = document.querySelectorAll(".answer");
+const questions = document.querySelectorAll(".faq_question")
 const questionListBody = document.getElementById("question_body");
 
 questionListBody.addEventListener("click", (e) => {
-if(e.srcElement.classList.contains("expand_btn")){
-  console.log("This is the expand button")
-} 
-if(e.srcElement.classList.contains("close_btn")){
-  console.log("This is the close button");
-}
-})
+  e.preventDefault();
+  console.log(e.target);
 
-expandButtons.forEach((expandButton) => {
-  expandButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    let answer = (e.srcElement.parentElement.parentElement.parentElement.querySelector(".answer"));
-    let expandImageSrc = (e.srcElement.attributes.src);
-    let expandElementClassLists = (e.srcElement.parentElement.classList);
-    expandElementClassLists.remove("expand_btn")
-    expandElementClassLists.add("close_btn")
-    answer.classList.remove("hidden")
-    expandImageSrc.value = "assets/images/icon-minus.svg";
-    console.log(expandElementClassLists);
-  })
-})
-closeButtons.forEach((closeButton) => {
-  closeButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    let answer = (e.srcElement.parentElement.parentElement.parentElement.querySelector(".answer"));
-    let expandImageSrc = (e.srcElement.attributes.src);
-    let expandElementClassLists = (e.srcElement.parentElement.classList);
-    expandElementClassLists.remove("close_btn")
-    expandElementClassLists.add("expand_btn")
-    answer.classList.add("hidden")
-    expandImageSrc.value = "assets/images/icon-plus.svg";
-    console.log(expandElementClassLists);
-  })
+  if (e.target.classList.contains("faq_question") || e.target.classList.contains("button")) {
+    const expandButton = e.target.nextElementSibling?.classList.contains("expand_btn") ?
+      e.target.nextElementSibling :
+      null;
+    const closeButton = e.target.nextElementSibling?.classList.contains("close_btn") ?
+      e.target.nextElementSibling :
+      null;
+
+    if (expandButton || expandButton.querySelector("img")) {
+      let answer = expandButton.parentElement.nextElementSibling
+      let expandImageSrc = expandButton.querySelector("img").attributes.src
+      let expandElementClassLists = expandButton.classList
+      console.log(expandElementClassLists);
+      expandElementClassLists.remove("expand_btn")
+      expandElementClassLists.add("close_btn")
+      answer.classList.remove("hidden")
+      expandImageSrc.value = "assets/images/icon-minus.svg";
+    }
+    if (closeButton || closeButton.querySelector("img")) {
+      let answer = closeButton.parentElement.nextElementSibling
+      let expandImageSrc = closeButton.querySelector("img").attributes.src
+      let expandElementClassLists = closeButton.classList
+      console.log(expandElementClassLists);
+      expandElementClassLists.remove("close_btn")
+      expandElementClassLists.add("expand_btn")
+      answer.classList.add("hidden")
+      expandImageSrc.value = "assets/images/icon-plus.svg";
+    }
+
+  }
 })
