b208002e5fdacfe95f4093e1a7e558b006f0c08f
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-28 04:07:09 +0100

    I think I'm almost done :

--- script/main.js
+++ script/main.js
@@ -6,31 +6,26 @@ const questionListBody = document.getElementById("question_body");
 
 questionListBody.addEventListener("click", (e) => {
   e.preventDefault();
-  // console.log(e.target);
 
   const targetElement = e.target.closest(".faq_question, .button");
 
   if (targetElement) {
-    const expandButton = targetElement.nextElementSibling?.classList.contains("expand_btn") 
-        ? targetElement.nextElementSibling 
-        : null;
-    const closeButton = targetElement.nextElementSibling?.classList.contains("close_btn") 
-        ? targetElement.nextElementSibling 
-        : null;
-    const button = targetElement?.classList.contains("button") 
-        ? targetElement 
-        : null;
+    const expandButton = targetElement.nextElementSibling?.classList.contains("expand_btn") ?
+      targetElement.nextElementSibling :
+      null;
+    const closeButton = targetElement.nextElementSibling?.classList.contains("close_btn") ?
+      targetElement.nextElementSibling :
+      null;
+    const button = targetElement.classList.contains("button") ? targetElement : null;
 
-        console.log(button);
 
-    if (expandButton || button) {
+
+    if (expandButton) {
       const answer = expandButton.parentElement.nextElementSibling;
       const expandImage = expandButton.querySelector("img");
-      console.log(answer);
 
       const expandImageSrc = expandImage.attributes.src;
       const expandElementClassLists = expandButton.classList;
-      console.log(expandElementClassLists);
 
       expandElementClassLists.remove("expand_btn");
       expandElementClassLists.add("close_btn");
@@ -38,17 +33,29 @@ questionListBody.addEventListener("click", (e) => {
       expandImageSrc.value = "assets/images/icon-minus.svg";
     }
 
-    if (closeButton || button) {
+    if (closeButton) {
       const answer = closeButton.parentElement.nextElementSibling;
 
       const expandImageSrc = closeButton.querySelector("img").attributes.src;
       const expandElementClassLists = closeButton.classList;
-      console.log(expandElementClassLists);
 
       expandElementClassLists.remove("close_btn");
       expandElementClassLists.add("expand_btn");
       answer.classList.add("hidden");
       expandImageSrc.value = "assets/images/icon-plus.svg";
+    }
+
+    if (button) {
+      const answer = button.parentElement.parentElement.parentElement.querySelector(".answer");
+      const isHidden = answer.classList.contains("hidden");
+
+      if (isHidden) {
+        answer.classList.remove("hidden");
+        button.src = "assets/images/icon-minus.svg";
+      } else {
+        answer.classList.add("hidden");
+        button.src = "assets/images/icon-plus.svg";
+      }
     }
   }
 });
